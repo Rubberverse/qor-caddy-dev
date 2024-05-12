2ac1fc01d48340abb579e0f2ff3a7f518df17df8
MrRubberDucky <mrd.mrrubberducky@gmail.com>
2024-05-12 19:07:53 +0200

    Sync current work to dev fork

--- scripts/array-helper.sh
+++ scripts/array-helper.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
-if [ "$GO_CADDY_VERSION" = "" ]; then
-    printf "[array-helper - warn] Empty value supplied for GO_CADDY_VERSION\n"
-    printf "[array-helper - warn] Falling back to master tag, might not build!\n"
-    export GO_CADDY_VERSION=master
+printf "[array-helper] Pre-eliminary checking of environmental variables - Pass 1/2\n"
+
+if ! [ "${CADDY_MODULES}" = "" ]; then
+    printf "[array-helper - Check 1] PASSED\n"
+else
+    printf "[array-helper - Check #1] FAILED: Empty value supplied for CADDY_MODULES\n"
+    printf "[array-helper - Check #1] FAILED: If this is intentional, pass 0 to CADDY_MODULES environmental variable to build Caddy without modules\n"
+    exit 2
 fi
 
-if [ "$GO_CADDY_VERSION" = "v2.7.6" ] || [ "$GO_CADDY_VERSION" = "2.7.6" ]; then
-    printf "[array-helper - warn] Falling back to 32f7dd4 commit in order to fix build failure for v2.6.7\n"
-    export GO_CADDY_VERSION=32f7dd4
-fi 
-
-if [ "$XCADDY_MODULES" = "" ]; then
-    printf "[array-helper - error] Empty value supplied for XCADDY_MODULES\n"
-    printf "[array-helper - error] If this is intentional, pass 0 to XCADDY_MODULES environmental variable to build Caddy without modules\n"
-    exit 1
-elif [ "$XCADDY_MODULES" = "0" ]; then
-    printf "[array-helper] User wants to build vanilla/stock Caddy (without extra modules)\n[array-helper - info] This process may take a while\n\n"
-    exec /app/go/bin/xcaddy build "${GO_CADDY_VERSION}" --output /app/bin/caddy
+if ! [ "${CADDY_TAG_VERSION}" = "" ] ; then
+    printf "[array-helper - Check #2] PASSED]\n"
+else
+    printf "[array-helper - Check #2] FAILED: Empty value supplied for CADDY_TAG_VERSION\n"
+    printf "[array-helper - Check #2] WARNING: Build process will fallback to master branch which may not build successfully\n"
+    export CADDY_TAG_VERSION=master
 fi
 
-printf "[array-helper] Parsing XCADDY_MODULES into an array\n\n"
-read -ra env_arr <<<"$XCADDY_MODULES"
+printf "[array-helper - init] Initializing variables\n"
+FILE_PATH=/app/caddy/main.go
+TEMP_FILE=/app/caddy/temp.go
+PROCESSED=false
 
-printf "[array-helper] Building out the command and then running it\n[array-helper] This process may take a while\n\n"
-cmd_array=( /app/go/bin/xcaddy build "${GO_CADDY_VERSION}" )
+printf "[array-helper - init] Creating necessary files\n"
+touch /app/caddy/temp.go
 
-for module in "${env_arr[@]}"; do
-    cmd_array+=( --with "$module" )
-done
+printf "[array-helper - init] Parsing CADDY_MODULES into Array\n"
+read -a CADDY_MODULES_ARRAY <<< "$CADDY_MODULES"
 
-cmd_array+=( --output /app/bin/caddy )
-"${cmd_array[@]}"
+printf "[array-helper - debug] Listing variables and file directories\n"
+printf "%b" "FILE_PATH: ${FILE_PATH}\n"
+printf "%b" "TEMP_FILE: ${TEMP_FILE}\n"
+printf "%b" "ARRAY PROCESSED: ${PROCESSED}\n"
+ls -l /app/caddy
+
+printf "[array-helper - init] Sanity check\n"
+echo -n "" > $TEMP_FILE
+
+printf "[array-helper] Adding modules to /app/caddy/main.go\n"
+
+while IFS= read -r line
+do
+    if [[ $line == *"// plug in extra Caddy modules here"* ]] && [ "$PROCESSED" = false ]; then
+        for module in "${CADDY_MODULES_ARRAY[@]}"
+            do
+                echo "\t_ \"$module\"" >> $TEMP_FILE
+        done        
+	PROCESSED=true
+    fi
+
+    printf "[array-helper] Writing module \"$module\" to file\n"
+    echo "$line" >> $TEMP_FILE 
+done < "$FILE_PATH"
+
+printf "[array-helper] Overwriting main.go with temporary file\n"
+mv $TEMP_FILE $FILE_PATH
+
+printf "[array-helper - debug] Show go module"
+cat $FILE_PATH
+cat $TEMP_PATH
 
 # https://unix.stackexchange.com/a/403401
 # https://stackoverflow.com/a/30212526
